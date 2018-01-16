cd12e0d701759bf20397a099defe323be93008ad
Raphael Yu <xianyu@microsoft.com>
2018-01-16 13:15:54 +0800

    Update azure-cli repo.

--- solution_template/scripts/install_jenkins.sh
+++ solution_template/scripts/install_jenkins.sh
@@ -227,8 +227,8 @@ fi
 
 sudo add-apt-repository ppa:openjdk-r/ppa --yes
 
-echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 sudo apt-get install apt-transport-https
 sudo apt-get update --yes
 
